46f4c39ba9d5c91d1ed50871c954ee03c6655b29
Francisco Javier Ruiz Rodriguez <Pajarrurro@gmail.com>
2016-10-14 14:38:38 +0200

    Falta configurar inkcanvas

--- 06-Multipaginas-Canvas/App.xaml.cs
+++ 06-Multipaginas-Canvas/App.xaml.cs
@@ -40,10 +40,10 @@ namespace _06_Multipaginas_Canvas
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
+            /*if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
-            }
+            }*/
 #endif
             Frame rootFrame = Window.Current.Content as Frame;
 
